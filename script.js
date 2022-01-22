dfc16b9785d24166f1e4049111f424a2dbf0d319
Paxxous <itsenveehio@gmail.com>
2022-01-22 10:50:13 -0500

    well now I'm trying to make a game lamo

--- script.js
+++ script.js
@@ -3,3 +3,36 @@ console.log("Welcome to the console.");
 for (let i = 100; i > 0; i--) {
   console.log("Loop test :D");
 }
+
+
+// movement part of the game :D
+document.addEventListener('keypress', function(key) {
+  var you = document.getElementById("you");
+  // console.log("key pressed")
+  var marginUp;
+  var marginSide;
+
+  if (key.key == "w") {
+    console.log("w");
+    // marginUp = marginUp + 1;
+    you.style.marginLeft = "auto";      
+  }
+  if (key.key == "a") {
+    console.log("a");    
+  }
+  if (key.key == "s") {
+    console.log("s");    
+  }
+  if (key.key == "d") {
+    console.log("d");    
+  }
+
+
+
+  // var posRight = key.offsetY;
+
+  // you.style.marginLeft = posLeft + 'px';
+  // you.style.marginTop = posRight + 'px';
+
+  // console.log(posLeft + posRight);
+});
