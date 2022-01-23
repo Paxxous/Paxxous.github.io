df427ef5781ba291dac5b33e97fcec1cba6ccae7
Paxxous <itsenveehio@gmail.com>
2022-01-23 10:09:43 -0500

    added a function that detects when you touch the enemy and then a certain song plays

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 console.log("Welcome to the console.");
 
-for (let i = 100; i > 0; i--) {
-  console.log("Loop test :D");
-}
 
+// Fetch some variables
 var up_down = 0;
 var right_left = 0;
 var you = document.getElementById("you");
+var enemy = document.getElementById("enmy");
 
+//randomly position the enemy
+enemy.style.right = Math.random() + 'px';
 
-// movement part of the game :D
+// movement part of game
 document.addEventListener('keydown', function(key) {
   // console.log(you.style.marginTop);
 
@@ -17,46 +18,67 @@ document.addEventListener('keydown', function(key) {
   if (key.key == "w" || key.key == "ArrowUp") {
     // console.log("w");
     up_down = up_down - 10;
-    you.style.marginTop = up_down + 'px';      
+    you.style.top = up_down + 'px';      
   }
 
   if (key.key == "a" || key.key == "ArrowLeft") {
     // console.log("a");
     right_left = right_left - 10;
-    you.style.marginLeft = right_left + 'px';    
+    you.style.left = right_left + 'px';    
   }
   
   if (key.key == "d" || key.key == "ArrowRight") {
     // console.log("d");
     right_left = right_left + 10;
-    you.style.marginLeft = right_left + 'px';       
+    you.style.left = right_left + 'px';       
   }
 
   if (key.key == "s" || key.key == "ArrowDown") {
     // console.log("s");
     up_down = up_down + 10;
-    you.style.marginTop = up_down + 'px';       
+    you.style.top = up_down + 'px';       
   }
 
 
   // Prevent the character from going off of the game
-  if (you.style.marginLeft == "580px" || you.style.marginLeft == "590px") {
-    you.style.marginLeft = "0px";
+  if (you.style.left == "580px" || you.style.left == "590px") {
+    you.style.left = "0px";
     right_left = 0;
   }
 
-  if (you.style.marginLeft == "-10px" || you.style.marginLeft == "-20px") {
-    you.style.marginLeft = "570px";
+  if (you.style.left == "-10px" || you.style.left == "-20px") {
+    you.style.left = "570px";
     right_left = 570;
   }
 
-  if (you.style.marginTop == "580px" || you.style.marginTop == "590px") {
-    you.style.marginTop = "0px";
+  if (you.style.top == "580px" || you.style.top == "590px") {
+    you.style.top = "0px";
     up_down = 0;
   }
 
-  if (you.style.marginTop == "-10px" || you.style.marginTop == "-20px") {
-    you.style.marginTop = "570px";
+  if (you.style.top == "-10px" || you.style.top == "-20px") {
+    you.style.top = "570px";
     up_down = 570;
   }
+
+  // sorry if this isn't the idea code
 });
+
+//Check if the player and the enemy is colliding to cause a game over. :(
+var isColliding = setInterval(function() {
+  //fetch both the player and the enemy's properties
+  var wherePlayer = you.getBoundingClientRect();
+  var whereEnemy = enemy.getBoundingClientRect();
+
+  //detect if player is touching enemy
+  var horizontal = wherePlayer.x < whereEnemy.x + whereEnemy.width && wherePlayer.x + wherePlayer.width > whereEnemy.x;
+  var vertical = wherePlayer.y < whereEnemy.y + whereEnemy.width && wherePlayer.y + wherePlayer.width > whereEnemy.y;
+
+  //
+  if (horizontal && vertical) {
+    var comard = new Audio("d/National-Anthem-Of-The-USSR.mp3");
+    comard.play();
+    setInterval(function(){}, 1000);
+    // window.location.reload()
+  }
+}, 10);
